deab349c1a9832e4f8e0c71077cd35c94a271bf3
unknown <leaf_cis@naver.com>
2019-07-10 12:13:17 +0900

    [DSM FESTIVAL] Add something

--- src/component/Modal.tsx
+++ src/component/Modal.tsx
@@ -11,11 +11,31 @@ const ModalWrapper = styled.div`
     align-items: center;
     justify-content: center;
 
-    div {
+    .wrapper {
         width: 34vw;
         height: 81vh;
         background-color: #ffffff;
         border-radius: 0.5rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+
+        h1 {
+            margin: 0 0 0 0;
+            font-family: 'Spoqa Han Sans';
+            font-size: 2rem;
+            font-weight: bold;
+            color: #365dff;
+        }
+
+        div {
+            height: 70vh;
+            background-color: #ffffff;
+            display: flex;
+            flex-direction: column;
+            width: 34vw;
+        }
     }
 `
 
@@ -26,8 +46,11 @@ interface Props {
 const Modal : FC<Props> = ({setState}) => {
     return (
         <ModalWrapper onClick = {() => setState(null)}>
-            <div onClick = {(e) => e.stopPropagation()}>
+            <div className = "wrapper" onClick = {(e) => e.stopPropagation()}>
+                <h1>이용내역</h1>
+                <div>
 
+                </div>
             </div>
         </ModalWrapper>
     );
