3573b623b1752c7dd5c10e95c29c0444be919a40
unknown <leaf_cis@naver.com>
2019-07-18 01:09:03 +0900

    [DSM FESTIVAL] 진짜 끝!!!!

--- src/container/ModalContainer.tsx
+++ src/container/ModalContainer.tsx
@@ -26,7 +26,6 @@ const ModalContainer : FC<Props> = ({state, setState}) => {
                             Authorization: `Bearer ${getCookie('token')}`
                         }
                     })
-                    console.log(result)
                     const key = Object.keys(result.data)
                     setList(result.data[key[0]].map((data) => {
                         return <ModalList key = {rank} state = {state} data = {data} rank = {rank++}/>
